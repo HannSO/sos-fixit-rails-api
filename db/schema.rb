5a5279e14ed4f40aeb55a21adda5949c29e3cf70
seanhawkridge <sean@hawkridge.co>
2016-03-26 11:49:30 +0000

    converting latlng to float; adding more properties into rabl templates for skills/users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325151805) do
+ActiveRecord::Schema.define(version: 20160326111612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 20160325151805) do
     t.json     "tokens"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "latitude"
-    t.string   "longitude"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 users = [
-  ['alf@mail.com', 'password'],
-  ['bert@mail.com', 'password'],
-  ['chris@mail.com', 'password'],
-  ['david@mail.com', 'password']
+  ['alf@mail.com', 'Alf Jones', 'password', 51.5285, 0.0847],
+  ['bert@mail.com', 'Bert Johnson', 'password', 51.5288, -0.0809],
+  ['chris@mail.com', 'Chris Pine', 'password', 51.5174, -0.083],
+  ['david@mail.com', 'David Edwards', 'password', 51.5195, -0.0611]
 ]
 
-users.each do |email, password|
-  User.create(email: email, password: password, password_confirmation: password)
+users.each do |email, name, password, lat, lng|
+  User.create(email: email, name: name, password: password, password_confirmation: password, latitude: lat, longitude: lng)
 end
 
 skills = ['plumbing','bike maintenance','gardening','car maintenance','english']
