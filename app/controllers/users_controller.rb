2c5d14a6b4e79b23662eee1d63960992324d14d2
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-03-28 14:33:05 +0100

    controller added to edit user with new skills

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit(user_params)
+    @user = User.find(params [:user])
+
+    @skill = Skill.find(params [:skill])
+    @user.skill << @skill
+  end
+
+  def user_params
+    params.require(:user).permit(:skills, :user)
+  end
 end
