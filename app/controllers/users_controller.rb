03bb6688bea5250dc26213e0e8bc6b2781e359ff
ZeshanRasul <zeshanrasul90@gmail.com>
2016-03-29 00:16:29 +0100

    Users can add pre-existing skills to themselves

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def edit(user_params)
-    @user = User.find(params [:user])
+  def edit
+    @user = User.find(params[:id])
+    # @skill = Skill.find(params [:skill])
+    # @user.skill << @skill
+  end
 
-    @skill = Skill.find(params [:skill])
-    @user.skill << @skill
+  def update
+    @user = User.find(params[:user])
+    @skill = Skill.find(params[:skill])
+    @user.skills << @skill
+    render json: @user
+
   end
 
   def user_params
     params.require(:user).permit(:skills, :user)
   end
+
 end
